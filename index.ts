9545aba34d7fb97ab7ce1291489310523aa2eb17
Saim Kamal <ubaidkamal62@gmail.com>
2024-03-24 22:35:01 -0700

    Created First time Number Guessin Game.

---
+++ index.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random()*5 + 1);
+
+const answer = await inquirer.prompt([{
+    name:"userGuessedNumber",
+    type:"number",
+    message:"Please Guess a number between 1 to 5 : ",
+},
+]);
+if(answer.userGuessedNumber === randomNumber){
+    console.log("``Congrulation! Your Guessed Number is Correct.``")
+} else{
+    console.log("``Try Again! Your Guessed Number is Wrong.``")
+}
